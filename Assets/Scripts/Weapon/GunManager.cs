bbcc6b2e1e8f30c34067f246ab20dec5b413ade0
WhiteDogLAPTOP <wodnr97@gmail.com>
2018-08-22 16:56:49 +0900

    Gun과 GunManager 내용 추가함.
    Gun에는 장탄에 관한 내용과 총알을 생성하는 구획을 추가함.
    GunManager는 Gun을 리스트로 받아 일괄적으로 발사명령
    할 수 있는 구문을 추가함.
    
    테스트는 아직 하지 않음.

--- Assets/Scripts/Weapon/GunManager.cs
+++ Assets/Scripts/Weapon/GunManager.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class GunManager : MonoBehaviour
 {
+    public List<Gun> guns = new List<Gun>();
 
+    private void Awake()
+    {
+        if(guns.Count ==0)
+        {
+            Debug.LogError("GunManager : guns 비어있음");
+        }
+    }
+
+    public void Fire()
+    {
+        for (int i = 0; i < guns.Count; i ++)
+        {
+            guns[i].StartCoroutine("Fire");
+        }
+    }
 }
