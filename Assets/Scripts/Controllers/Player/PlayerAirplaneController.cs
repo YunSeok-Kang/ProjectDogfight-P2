9a101fc599a1b91640699e0175a22f1716daa9ff
YS <ysck73@naver.com>
2018-08-27 01:30:09 +0900

    플레이어 비행기 컨트롤러 추가
    플레이어의 Pitch 입력을 받아 그에 맞게 비행기를 상승/하강하는 기능 추가

--- Assets/Scripts/Controllers/Player/PlayerAirplaneController.cs
+++ Assets/Scripts/Controllers/Player/PlayerAirplaneController.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Airplane))]
 public class PlayerAirplaneController : PlayerController
 {
+    private Airplane _playerAirplane = null;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+		if (_playerAirplane == null)
+        {
+            _playerAirplane = gameObject.GetComponent<Airplane>();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        //CrossPlatformInputManager.GetAxis("Vertical");
+        //bool airBrakes = CrossPlatformInputManager.GetButton("Fire1");
+    }
+
+    float GetUserPitch()
+    {
+        float pitchingValue = Input.GetAxis("Vertical");
+        pitchingValue = Mathf.Clamp(pitchingValue, -1, 1);
+
+        Debug.Log("Pitching Value: " + pitchingValue);
+        return pitchingValue;
+
+        //CrossPlatformInputManager.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        float pitchingValue = GetUserPitch();
+
+        _playerAirplane.Move(pitchingValue, 1, false);   
+    }
 }
