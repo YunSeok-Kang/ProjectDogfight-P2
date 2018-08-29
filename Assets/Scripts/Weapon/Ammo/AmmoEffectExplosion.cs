dae7a182135c37990ca93aa73588ef52192f3409
WhiteDogDESKTOP <wodnr97@gmail.com>
2018-08-29 12:52:30 +0900

    시한신관과 폭발 스크립트를 추가함
    시한신관 역할을 하는 스크립트를 구현함.
    폭발 스크립트는 추가하여 이펙트를 출력케만 하고,
    폭발에 따른 데미지(스플래시 데미지 등)은 미구현함.

---
+++ Assets/Scripts/Weapon/Ammo/AmmoEffectExplosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoEffectExplosion : AmmoEffect
+{
+    public ParticleSystem explosionEffect;
+
+    public override void Effect()
+    {
+        Explosion();
+        base.Effect();
+    }
+    protected virtual void Explosion()
+    {
+        var particleEffect = Instantiate(explosionEffect, transform.position, transform.rotation);
+        float totalDuration = particleEffect.main.duration + particleEffect.subEmitters.GetSubEmitterSystem(0).main.duration;
+        Destroy(particleEffect.gameObject, totalDuration);
+    }
+}
---
+++ Assets/Scripts/Weapon/Ammo/AmmoTriggerTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoTriggerTime : AmmoTrigger
+{
+    public float triggerTime;
+
+    private void Start()
+    {
+        StartCoroutine("EffectAfterTime");
+    }
+    IEnumerator EffectAfterTime()
+    {
+        yield return new WaitForSeconds(triggerTime);
+        Trigger();
+    }
+}
