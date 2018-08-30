de2cd46a8c7c90b7645079f871b4cf88b540f794
WhiteDogDESKTOP <wodnr97@gmail.com>
2018-08-30 20:56:31 +0900

    Ammo 파괴시 이펙트 출력 추가
    Ammo가 파괴될 때 특정한 파티클을
    출력하도록 구현함.

--- Assets/Scripts/Weapon/Ammo/Ammo.cs
+++ Assets/Scripts/Weapon/Ammo/Ammo.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class Ammo : Weapon
 {
     public float lifetime = 5f;
-    public ParticleSystem ammoDestroyEffect = null;
     [HideInInspector]
     public string enemyTag;
+    public ParticleSystem ammoDestructionParticle = null;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         VoxDestroy(lifetime);
-	}
-    protected override void VoxDestroy(float destroyDelay = 0)
+    }
+    protected override void VoxDestroyLogic()
     {
-        if (ammoDestroyEffect != null)
+        if (ammoDestructionParticle != null)
         {
-            var particleEffect = Instantiate(ammoDestroyEffect, transform.position, transform.rotation);
-            float totalDuration = particleEffect.main.duration + particleEffect.subEmitters.GetSubEmitterSystem(0).main.duration;
+            var particleEffect = Instantiate(ammoDestructionParticle, transform.position, transform.rotation);
+            float totalDuration = particleEffect.main.duration;
+            for (int i = 0; i < particleEffect.subEmitters.subEmittersCount; i++)
+            {
+                totalDuration += particleEffect.subEmitters.GetSubEmitterSystem(i).main.duration;
+            }
             Destroy(particleEffect.gameObject, totalDuration);
         }
-        base.VoxDestroy(destroyDelay);
+        base.VoxDestroyLogic();
     }
-
 }
