c1adb8fc063f765eab90fe2f35966ac0ed2c5c88
YS <ysck73@naver.com>
2018-09-03 15:40:05 +0900

    Restart 버튼 추가

---
+++ Assets/Scripts/UI/RestartButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestartButton : MonoBehaviour 
+{
+    public void ButtonPressed()
+    {
+        GameManager.Instance.RestartGame();
+    }
+}
