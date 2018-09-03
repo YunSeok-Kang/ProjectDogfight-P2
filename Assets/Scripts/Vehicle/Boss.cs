f34f44dc1ffdbc2e0bd09bc0c1b32856a3b47617
YS <ysck73@naver.com>
2018-09-03 15:29:06 +0900

    Boss에 Activate 기능 추가
    Boss를 Activate 하면 하위의 GunManager들도 모두 On/Off 됨.

--- Assets/Scripts/Vehicle/Boss.cs
+++ Assets/Scripts/Vehicle/Boss.cs
@@ -2,6 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss : Vehicle {
+public class Boss : Vehicle
+{
+    protected override void OnActivateObject()
+    {
+        GunManager[] gunManagers = gameObject.GetComponentsInChildren<GunManager>();
+        foreach (GunManager gunManager in gunManagers)
+        {
+            gunManager.enabled = true;
+        }
+    }
 
+    protected override void OnUnActivateObject()
+    {
+        GunManager[] gunManagers = gameObject.GetComponentsInChildren<GunManager>();
+        foreach (GunManager gunManager in gunManagers)
+        {
+            gunManager.enabled = false;
+        }
+    }
 }
